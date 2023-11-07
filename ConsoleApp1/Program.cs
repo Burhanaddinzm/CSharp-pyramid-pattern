c7162fc76129c6b584e85e9ffd488b1f6fd7a784
Burhanaddinzm <burhanaddinzm@code.edu.az>
2023-11-07 13:17:16 +0400

    Pyramid Pattern - for loop

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿int n, x, y, z;
+Console.WriteLine("Input row count:");
+int.TryParse(Console.ReadLine(), out n);
+if (n <= 60)
+{
+    for (x = 1; x <= n; x++)
+    {
+        for (y = 1; y <= n - x; y++)
+        {
+            Console.Write(" ");
+        }
+        for (z = 1; z <= x; z++)
+        {
+            Console.Write("* ");
+        }
+        Console.WriteLine();
+    }
+}
+else
+    Console.WriteLine("Can't input more that 60 rows!");
